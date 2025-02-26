80f7abdf11d53385a9e2c223b1ace7164b5ce741
CarsonTanner <you@example.com>
2025-02-26 14:05:58 -0700

    Add project files.

---
+++ Mission08_Team9/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Team9.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
